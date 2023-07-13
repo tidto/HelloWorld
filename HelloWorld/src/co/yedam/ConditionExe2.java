8de4f8d2a699f628e94cafee7008842e93519f77
tidto <139437094+tidto@users.noreply.github.com>
2023-07-13 22:47:13 +0900

    Add files via upload

--- HelloWorld/src/co/yedam/ConditionExe2.java
+++ HelloWorld/src/co/yedam/ConditionExe2.java
@@ -9,16 +9,26 @@ public class ConditionExe2 {
 			System.out.println("메뉴를 선택하세요(숫자를 입력)>> ");
 			System.out.println("1.로그인 2.로그아웃 3.회원가입 4.종료");
 			String menu = scn.nextLine();
+						
+			if(Integer.parseInt(menu) == 1) {
+				System.out.println("로그인메뉴입니다.");break;
+			}else if(Integer.parseInt(menu) == 2) {
+				System.out.println("로그아웃메뉴입니다.");break;
+			}else if(Integer.parseInt(menu) == 3) {
+				System.out.println("회원가입메뉴입니다.");break;
+			}else {
+				System.out.println("종료합니다."); break;
+				
+			}
 			
-			
-			
-			
-			//switch if- else if- else
-			// "1" =>equals
-		}
-		System.out.println("end of prog.");
+			//end
 		
-	}// end of main();
+			
+		}
+		System.out.println("end of prog.");	
+	}
+
+	//end of main();
 	
 	public static void login() {
 		System.out.println("로그인메뉴입니다.");
