67ccdb703c20e4d09d906cfa5a0f1bf530835c4f
admin <admin@DESKTOP-EMENTOF>
2023-07-13 17:13:45 +0900

    cmomo

--- HelloWorld/src/co/yedam/ConditionExe.java
+++ HelloWorld/src/co/yedam/ConditionExe.java
@@ -2,6 +2,14 @@ package co.yedam;
 
 public class ConditionExe {
 	public static void main(String[] args) {
+		//Math
+		for(int i=0; i<10; i++) {
+		int result = (int)(Math.random() * 6)+1 ; // 0~1 사이의 임의의 값.
+		System.out.println(result);
 		
+		if(result % 2 == 0) {
+			System.out.println("2의 배수입니다.");
+		}
+		}
 	}
 }
