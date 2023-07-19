7649024abd92b6579c87aa94d1301428c7eaeb84
tidto <139437094+tidto@users.noreply.github.com>
2023-07-20 00:52:29 +0900

    Add files via upload

---
+++ T20230719/src/co/yedam/Ex01.java
@@ -0,0 +1,16 @@
+package co.yedam;
+
+import java.util.Scanner;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("num1>>");
+		int num1 = Integer.parseInt(scn.nextLine());
+		System.out.println("num2>>");
+		int num2 = Integer.parseInt(scn.nextLine());
+		int res = (num1 > num2) ? num1: num2;
+		
+		System.out.println(res);
+	}
+}
---
+++ T20230719/src/co/yedam/Ex02.java
@@ -0,0 +1,29 @@
+package co.yedam;
+
+import java.util.Scanner;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("num1>>");
+		int num1 = Integer.parseInt(scn.nextLine());
+		System.out.println("num2>>");
+		int num2 = Integer.parseInt(scn.nextLine());
+		
+		int sum2 = 0;
+		int sum3 = 0;
+		
+		int first = (num1 < num2) ? num1 : num2;
+		int end = (num1 < num2) ? num2 : num1;
+		for (int i = first; i <=end; ++i) {
+			if (i % 2 == 0) {
+				sum2 += i;
+			}
+			if (i % 3 == 0) {
+				sum3 += i;
+			}
+		}
+		
+		System.out.printf("sum2: %d, sum3: %d\n",sum2,sum3);
+	}
+}
